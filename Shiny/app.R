4f162675543e6d7a226918ed46f4d461ffff509e
Jasper Koehorst <>
2022-02-28 18:23:03 +0100

    init 3d structure

--- Shiny/app.R
+++ Shiny/app.R
@@ -9,6 +9,9 @@
 
 library(shinydashboard)
 library(DT)
+# devtools::install_github("swsoyee/r3dmol")
+library(r3dmol)
+
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
     # Application title
@@ -36,15 +39,19 @@ ui <- dashboardPage(
         tabItem(tabName = "overview", h2("Overview")),
         
         # third tab content
-        tabItem(tabName = "3dstructures", h2("3D tab content"))
-        )
-    )
-)
+        tabItem(
+            tabName = "3dstructures", 
+            h2("3D tab content"), 
+            fluidRow(column(12, div(DT::dataTableOutput("pdbTable")))),
+            fluidRow(column(12, r3dmolOutput("pdb")))
+         )
+)))
     
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
         # Loads the time series data
         timeSeries <- data.table::fread("./data/timeseries.tsv",header = TRUE, sep = "\t")
+        pdbList <- list.files("./data/pdb", full.names = TRUE)
         
         output$overviewTable <- DT::renderDataTable(
             datatable(
@@ -69,6 +76,51 @@ server <- function(input, output, session) {
             ggplot(df_melted, aes(x = variable, y = value)) + geom_line(aes(color = Proteins, group = Proteins))
         }
     })
+    
+    output$pdbTable <- DT::renderDataTable(datatable(
+        data=as.data.frame(pdbList),
+        # extensions = 'Buttons',
+        filter = 'top',
+        selection = 'single',
+        rownames= FALSE,
+        options = list()
+        ))
+    
+    output$pdb <- renderR3dmol({
+        s = input$pdbTable_rows_selected
+        if (!is.null(s)) {
+            pdb_file = pdbList[s]
+            r3dmol(
+                viewer_spec = m_viewer_spec(
+                    cartoonQuality = 10,
+                    lowerZoomLimit = 50,
+                    upperZoomLimit = 350
+                ),
+                # id = "demo2",
+                # elementId = "demo2"
+            ) %>%
+                # Add model to scene
+                m_add_model(data = pdb_file, format = "pdb") %>%
+                # Zoom to encompass the whole scene
+                m_zoom_to() %>%
+                # Set style of structures
+                m_set_style(style = m_style_cartoon(color = "#00cc96")) %>%
+                # Set style of specific selection (selecting by secondary)
+                m_set_style(
+                    sel = m_sel(ss = "s"),
+                    style = m_style_cartoon(color = "#636efa", arrows = TRUE)
+                ) %>%
+                # Style the alpha helix
+                m_set_style(
+                    sel = m_sel(ss = "h"), # Style alpha helix
+                    style = m_style_cartoon(color = "#ff7f0e")
+                ) %>%
+                # Rotate the scene by given angle on given axis
+                m_rotate(angle = 90, axis = "y") %>%
+                # Animate the scene by spinning it
+                m_spin()
+        }
+        })
 }
     
     # Run the application
